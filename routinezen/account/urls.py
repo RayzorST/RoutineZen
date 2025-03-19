492a12ccd229a61d52bc73dbb5ee98893d845fea
RayzorST <116524907+RayzorST@users.noreply.github.com>
2025-03-19 17:45:57 +0300

    add redirect

--- routinezen/account/urls.py
+++ routinezen/account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.account),
     path('login/', views.LoginFormView.as_view()),
     path('registration/', views.RegistrationFormView.as_view()),
     path('me=<int:pk>', views.MeDetailView.as_view()),
--- routinezen/account/views.py
+++ routinezen/account/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import DeleteView
 from django.views.generic.edit import FormView
 from django.contrib.auth.models import User
 from .forms import RegistrationFrom, LoginForm
+
+def account(request):
+    if request.user.is_authenticated:
+        return redirect(f"/account/me={request.user.id}")
+    return redirect("/account/login")
 
 class RegistrationFormView(FormView):
     template_name = "main\\registration.html"
@@ -11,9 +17,11 @@ class RegistrationFormView(FormView):
     success_url = "/account/me="
 
     def form_valid(self, form):
+        ### НЕТ ПРОВЕРОК
         data = form.cleaned_data
         user = User.objects.create_user(data["username"], data["email"], data["password"])
         user.save()
+        login(self.request, user)
         self.success_url += str(user.id)
         return super().form_valid(form)
     
@@ -23,9 +31,13 @@ class LoginFormView(FormView):
     success_url = "/account/me="
 
     def form_valid(self, form):
+        ### НЕТ ПРОВЕРОК
+        self.request.user.is_authenticated
         data = form.cleaned_data
         user = authenticate(username=data["username"], password=data["password"])
+        login(self.request, user)
         self.success_url += str(user.id)
+        self.request.session["userid"] = user.id
         return super().form_valid(form)
 
 class MeDetailView(DeleteView):
